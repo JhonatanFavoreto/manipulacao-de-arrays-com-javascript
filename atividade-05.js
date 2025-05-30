fa63f3d70d201e656a42f686a9c80e7011b5f7d7
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:39:21 -0300

    Criar lista dos importados, e remover o primeiro importado com shift - atividade-05.js

---
+++ atividade-05.js
@@ -0,0 +1,11 @@
+// 5 – Erro de importação
+let importados = [
+  "carregador portátil",
+  "cabo HDMI",
+  "leitor biométrico",
+  "controle sem fio",
+  "webcam 4K"
+];
+let importadoRemovido = importados.shift();
+console.log(`Nova lista dos importados: ${importados}.`);
+console.log(`Importado removido: ${importadoRemovido}.`);
