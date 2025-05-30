e87a9b3d8837cacb456053620ef06a61dd77430c
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:55:00 -0300

    Criar fila do atendimento, e acrescenar os membros VIPs no topo da lista com unshift - atividade-07.js

---
+++ atividade-07.js
@@ -0,0 +1,10 @@
+let filaAtendimento = [
+  "cliente 003",
+  "cliente 004",
+  "cliente 005",
+  "cliente 006",
+  "cliente 007"
+];
+filaAtendimento.unshift("cliente Vip 001");
+filaAtendimento.unshift("cliente Vip 002");
+console.log(`Fila de atendimento: ${filaAtendimento}.`);
