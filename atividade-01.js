ffbc3ab06c77f8cdcac1e4d28a3e109b8eff8716
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:10:49 -0300

    Criar lista de promoções com push - atividade-01.js

---
+++ atividade-01.js
@@ -0,0 +1,14 @@
+// 1 – Cadastro de novos produtos
+let promocoes = [
+  "teclado mecânico",
+  "mouse gamer",
+  "monitor ultrawide",
+  "cadeira ergonômica",
+  "webcam HD",
+  "fone bluetooth",
+  "notebook i5",
+  "hub USB"
+];
+promocoes.push('SSD 1TB')
+promocoes.push('mesa digitalizadora')
+console.log(`Nova lista de promoções: ${promocoes}.`)
