0a2ffca4354d426fda8a0a91f6d7a5dc5231eb26
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:18:43 -0300

    Criar lista do cardápio com push - atividade-02.js

---
+++ atividade-02.js
@@ -0,0 +1,13 @@
+// 2 – Expansão de cardápio
+let cardapio = [
+  "pizza de calabresa",
+  "lasanha",
+  "sushi",
+  "salada Caesar",
+  "pastel",
+  "yakisoba",
+  "feijoada"
+];
+cardapio.push('hambúrguer artesanal')
+cardapio.push('wrap de frango')
+console.log(`Nova lista do cardápio: ${cardapio}.`)
