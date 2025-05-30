82181800379301c536a39913e8b6d665e2001b36
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 16:07:44 -0300

    Criar lista da chamada, e transferir dois alunos com unshift - atividade-08.js

---
+++ atividade-08.js
@@ -0,0 +1,11 @@
+// 8 – Alunos transferidos
+let chamada = [
+    "Carlos",
+    "Fernanda",
+    "João",
+    "Mariana",
+    "Lucas",
+    "Camila"
+  ];
+let alunosTransferidos = chamada.unshift("Beatriz", "Rafaela");
+console.log(`Nova chamada dos alunos: ${chamada}.`);
