b0a4b2d6cf9ecd0d9bb5124b3b1f8e9086fa8f0e
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:26:06 -0300

    Criar lista da nova produção, e removendo o último item com pop - atividade-03.js

---
+++ atividade-03.js
@@ -0,0 +1,12 @@
+//3 – Último produto com defeito
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+];
+let producaoRemovida = producao.pop();
+console.log(`Nova produção: ${producao}.`);
+console.log(`Item removido: ${producaoRemovida}.`);
