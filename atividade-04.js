975e42391ebf2b5cf8febdf54961716f0d7e8ff8
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 11:30:58 -0300

    Criar lista dos inscritos, e removendo o último inscrito com pop - atividade-04.js

--- atividade-04.js
+++ atividade-04.js
@@ -1,1 +1,7 @@
 // 4 – Cancelamento de última inscrição
+let inscritos = [
+  "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+];
+let inscritoRemovido = inscritos.pop();
+console.log(`Nova lista de inscritos: ${inscritos}.`);
+console.log(`Inscrito removido: ${inscritoRemovido}.`);
