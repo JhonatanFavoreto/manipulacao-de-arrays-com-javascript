6c6a5b821a154fb41cecb1c4d32beb15fb3544f7
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:40:46 -0300

    Criar atividade-09.js fazer conferência

---
+++ atividade-09.js
@@ -0,0 +1,15 @@
+/ 9 – Conferência de presença
+
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+];
+
+if (presenca.includes("Amanda Souza")) {
+  console.log("Amanda Souza compareceu à reunião.");
+} else {
+  console.log("Amanda Souza não compareceu à reunião.");
+}
