30fa4fcd2451ba745650b6f02a299859d7656825
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:42:14 -0300

    Criar atividade-10.js e verificação de produto

---
+++ atividade-10.js
@@ -0,0 +1,13 @@
+//10 – Verificação de produto em estoque
+let estoque = [
+  "mesa para PC",
+  "cadeira gamer",
+  "monitor 27 polegadas",
+  "hub USB",
+  "mouse sem fio"
+];
+if (estoque.includes("cadeira gamer")) {
+  console.log("A cadeira gamer está disponível em estoque.");
+} else {
+  console.log("A cadeira gamer está indisponível no momento.");
+}
