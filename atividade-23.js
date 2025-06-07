e04606a0d2f796fbd6a00eaa4e37a3a710898c4e
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:59:26 -0300

    Criar atividade-23.js e organizar lista

---
+++ atividade-23.js
@@ -0,0 +1,14 @@
+// 23 – Organizar lista de candidatos
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+candidatos.sort();
+console.log("Lista de candidatos organizada:");
+console.log(candidatos);
