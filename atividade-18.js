89d89b49e47a461bbe3ec6a57f711097c7500bc5
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:52:10 -0300

    Criar atividade-18.js e catálogo unificado

---
+++ atividade-18.js
@@ -0,0 +1,17 @@
+// 18 – Catálogo unificado
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+];
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+];
+// Usando concat
+let catalogoUnificado = hardware.concat(perifericos);
+console.log("Catálogo unificado:");
+console.log(catalogoUnificado);
