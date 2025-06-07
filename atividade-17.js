f7e5ce1f805cf9bda58a678b5a1bfaf214d31d20
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:51:12 -0300

    Criar atividade-17.js e atualização da lista

---
+++ atividade-17.js
@@ -0,0 +1,17 @@
+// 17 – Atualização de lista de usuários
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+];
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+];
+// Usando concat
+let usuariosCombinados = usuariosSistemaA.concat(usuariosSistemaB);
+console.log("Lista combinada de usuários:");
+console.log(usuariosCombinados);
