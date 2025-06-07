78f8d9da4313e3a7f87a4f2c203c5fd444c0dd4f
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 20:44:33 -0300

    Criar atividade-12.js e tags para URL

---
+++ atividade-12.js
@@ -0,0 +1,10 @@
+// 12.Tags para URL
+let palavrasChave = [
+  "desenvolvimento",
+  "frontend",
+  "javascript",
+  "css",
+  "react"
+];
+let urlBusca = palavrasChave.join("-");
+console.log("URL gerada: " + urlBusca);
