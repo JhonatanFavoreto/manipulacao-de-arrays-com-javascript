128c261bce70691029fa8cc58491aaec5e420454
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 21:00:23 -0300

    Criar atividade-24.js e ranking de vendas

---
+++ atividade-24.js
@@ -0,0 +1,5 @@
+// 24 – Ranking de vendas
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+vendas.sort((a, b) => b - a);
+console.log("Ranking de vendas (maior para menor):");
+console.log(vendas);
