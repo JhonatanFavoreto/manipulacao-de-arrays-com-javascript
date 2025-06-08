3a5d8a6e180e1cc56c08efcd5fac6f1e6f9616e1
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 21:05:51 -0300

    Criar atividade-26.js e histórico de navegação

---
+++ atividade-26.js
@@ -0,0 +1,12 @@
+//26 – Histórico de navegação
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+let atualizacoesNavegacao = historico.reverse();
+console.log("O último acesso do usuário", atualizacoesNavegacao);
