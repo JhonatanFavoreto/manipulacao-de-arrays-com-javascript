8556eb7bb986e25008e3d37d2d824b36389eaf53
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-06-07 21:03:10 -0300

    Criar atividade-25.js e atualização do sistema

---
+++ atividade-25.js
@@ -0,0 +1,13 @@
+// 25 – Últimas atualizações do sistema
+let atualizacoes = [
+  "Backup diário concluído",
+  "Servidor reiniciado",
+  "Nova versão implantada",
+  "Logs exportados",
+  "Firewall atualizado",
+  "Monitoramento ativado",
+  "Banco de dados otimizado",
+  "Script de segurança executado"
+];
+let atualizacoesMaisRecentes = atualizacoes.reverse();
+console.log(`Atualizações mais recentes: ${atualizacoesMaisRecentes}`);
